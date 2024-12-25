720c774fb058d0714f3e4d7eef6de3b9b077ee3a
mxgic1337 <60188749+mxgic1337@users.noreply.github.com>
2024-12-26 00:44:36 +0100

    feat: updated icon links to use <a> instead of <div>

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -2,9 +2,7 @@ import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const IconLink = ({icon, title, url}: { icon: IconDefinition, title: string, url?: string }) => {
-	return <div className={`icon-link${url ? " clickable" : ""}`} title={title} onClick={() => {
-		if (url) window.open(url, '_blank')
-	}}>
+	return <a className={`icon-link${url ? " clickable" : ""}`} title={title} href={url} target={'_blank'}>
 		<FontAwesomeIcon icon={icon}/>
-	</div>
+	</a>
 }
