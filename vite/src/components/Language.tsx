3ab919d99be5e80bfe054efd56d915f541413fd4
mxgic1337_ <60188749+mxgic1337@users.noreply.github.com>
2025-03-27 19:18:18 +0100

    feat(website): add wakatime language stats (#10)

--- vite/src/components/Language.tsx
+++ vite/src/components/Language.tsx
@@ -1,34 +1,45 @@
+import { nfIcons } from '../utils/language_icons.tsx';
 import { IconLink } from './IconLink.tsx';
 
 export const Language = ({
-  icon,
   text,
   learning,
   libraries,
+  stats,
   accent,
 }: {
-  icon?: string;
   text: string;
   learning?: boolean;
   libraries?: string[];
+  stats?: { data: { name: string; text: string }[] };
   accent?: string;
 }) => {
+  function getTime(language: string) {
+    if (!stats) return undefined;
+    return stats.data.find(
+      (lang) => lang.name.toLowerCase() === language.toLowerCase()
+    );
+  }
+
   return (
     <div className={'language'}>
       <p>
-        <img
-          src={`https://cdn.simpleicons.org/${icon || text.toLowerCase()}/${accent || 'currentColor'}`}
-        />{' '}
-        {text}
+        <span className={'icon'}>{nfIcons[text.toLowerCase()]}</span> {text}
         {learning && <span className={'learning'}>Learning</span>}
+        {stats ? (
+          <span className={'time'}>({getTime(text)?.text})</span>
+        ) : (
+          <span className={'time'}>(...)</span>
+        )}
       </p>
       <div style={{ display: 'flex' }}>
         {libraries &&
           libraries.map((library) => {
+            const time = getTime(library);
             return (
               <IconLink
-                iconName={library.toLowerCase()}
-                title={library}
+                icon={nfIcons[library.toLowerCase()]}
+                title={`${library}${time ? ` | ${time.text}` : ''}`}
                 accent={accent}
               />
             );
