99662cb4574d683b7654032e64baa1963d190158
mxgic1337 <60188749+mxgic1337@users.noreply.github.com>
2025-05-19 22:06:33 +0200

    feat: added sponsors section

--- public/script.js
+++ public/script.js
@@ -47,6 +47,15 @@ function setTheme(theme) {
   }
 }
 
+function escapeHTML(text) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const switcher = document.getElementById('theme-switcher');
   const defaultTheme = localStorage.getItem('flavor') || 'mocha';
@@ -56,6 +65,7 @@ window.addEventListener('DOMContentLoaded', () => {
   switcher.value = defaultTheme;
   setTheme(defaultTheme);
 
+  /* WakaTime total time */
   fetch(
     'https://wakatime.com/share/@mxgic1337/91258e31-1dd0-41f7-9f9c-c1c5cbc30f46.json'
   ).then(async (response) => {
@@ -69,4 +79,32 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+
+  /* GitHub Sponsors */
+  fetch('/api/sponsors').then(async (response) => {
+    if (response.ok) {
+      const sponsors = await response.json();
+      const sponsorsDiv = document.getElementsByClassName('sponsors')[0];
+      sponsorsDiv.innerHTML = '';
+      for (const sponsor of sponsors) {
+        const sponsorAvatar = document.createElement('img');
+        sponsorAvatar.classList.add('sponsor');
+        sponsorAvatar.src = sponsor.avatar;
+        sponsorAvatar.title = `${sponsor.name ? `${sponsor.name} (${sponsor.username})` : sponsor.username} • ${sponsor.tier.name}`
+        sponsorAvatar.onclick = () => {
+          window.open(`https://github.com/${sponsor.username}`, '_blank')
+        }
+        sponsorsDiv.appendChild(sponsorAvatar);
+      }
+      const sponsorAdd = document.createElement('div');
+      sponsorAdd.innerText = '♥';
+      sponsorAdd.classList.add('sponsor');
+      sponsorAdd.classList.add('add');
+      sponsorAdd.title = `Support me on GitHub Sponsors`
+      sponsorAdd.onclick = () => {
+        window.open(`https://github.com/sponsors/mxgic1337`, '_blank')
+      }
+      sponsorsDiv.appendChild(sponsorAdd);
+    }
+  })
 })
