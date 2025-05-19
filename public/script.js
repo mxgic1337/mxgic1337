efaa178ea26eca60692645ea31fc753b1e7c5274
mxgic1337 <60188749+mxgic1337@users.noreply.github.com>
2025-05-19 13:01:24 +0200

    feat: added theme switcher

--- public/script.js
+++ public/script.js
@@ -7,6 +7,7 @@ const ICONS = [
   { id: "less", name: "Less", icon: "" },
   { id: "sass", name: "Sass", icon: "" },
   { id: "css", name: "CSS", icon: "" },
+  { id: "html", name: "HTML", icon: "" },
   { id: "rust", name: "Rust", icon: "" },
   { id: "website", name: "Website", icon: "" },
 ]
@@ -27,3 +28,31 @@ function addIconCharacter(id) {
     parentNode.title = icon.name;
   }
 }
+
+/*
+ * Theme switcher
+ */
+
+let currentTheme = 'mocha';
+
+function setTheme(theme) {
+  currentTheme = theme;
+  document.getElementsByTagName('html')[0].className = theme;
+  localStorage.setItem('flavor', theme);
+  const wakaTimeBadges = document.getElementsByClassName('wakatime-badge');
+  const accentColor = window.getComputedStyle(document.body).getPropertyValue('--accent');
+
+  for (const badge of wakaTimeBadges) {
+    badge.src = badge.src.substring(0, badge.src.length - 6) + accentColor.substring(1)
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const switcher = document.getElementById('theme-switcher');
+  const defaultTheme = localStorage.getItem('flavor') || 'mocha';
+  switcher.addEventListener('change', (e) => {
+    setTheme(e.currentTarget.value)
+  });
+  switcher.value = defaultTheme;
+  setTheme(defaultTheme);
+})
